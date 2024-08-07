d3b91d9651460a5b43e7b2b2e17c282a162fa792
Kelvin Chuchu <mburukelvin17@gmail.com>
2024-08-08 02:16:16 +0300

    Creates a user table that enumerate the  country column

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Creates a table users following defined requirements
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255) UNIQUE,
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
